4011d8e71c099cbe651a0e53a0302b37f29ced95
Khushi K <169142716+Khushi-58@users.noreply.github.com>
2026-08-24 12:04:04 +0530

    Merge_Sort

--- lab2.c
+++ lab2.c
@@ -13,7 +13,7 @@ void merge(int arr[], int left, int mid, int right)
     int n2 = right - mid;
 
     int *L = (int *)malloc(n1 * sizeof(int));
-    int *R = (int *)malloc(n2 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int)); 
 
     for (i = 0; i < n1; i++)
         L[i] = arr[left + i];
